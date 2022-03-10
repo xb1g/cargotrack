bfb2db30fc6d7b840539e09d33dcc763e0707878
Bunyasit Fang <big168bk@gmail.com>
2022-03-10 14:48:07 +0700

    merge again?

--- src/services/auth/AuthContext.tsx
+++ src/services/auth/AuthContext.tsx
@@ -81,6 +81,18 @@ export const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
         ...userInfo,
       };
     });
+    console.log(auth);
+    // createUserWithEmailAndPassword(auth, email, password);
+    // .then((userCredential) => {
+    //   // Signed in
+    //   const user = userCredential.user;
+    //   // ...
+    // })
+    // .catch((error) => {
+    //   const errorCode = error.code;
+    //   const errorMessage = error.message;
+    //   // ..
+    // });
   };
 
   const onLogout = () => {
